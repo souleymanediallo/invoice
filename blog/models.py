865e52a48b1c070edd5f1097be2abd1d4093ba9d
souleymanediallo <souleymanedialloo@yahoo.fr>
2023-12-22 22:08:00 +0100

    add blog

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    date = models.DateField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
